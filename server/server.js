8f5725cc192ea1faad06e1fcbf354dda71b4a203
mohammedsuhail364 <mohammedthanish364@gmail.com>
2025-02-07 11:43:42 +0530

    new one

--- server/server.js
+++ server/server.js
@@ -5,53 +5,63 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth-routes");
 const mediaRoutes = require("./routes/instructor-routes/mediaRoutes");
 const instructorCourseRoutes = require("./routes/instructor-routes/courseRoutes");
-const getStudentViewCourseRoutes=require('./routes/student-routes/course-routes');
-const studentViewOrderRoutes=require('./routes/student-routes/order-routes');
-const studentCoursesRoutes=require('./routes/student-routes/student-courses-routes');
-const studentCourseProgressRoutes=require('./routes/student-routes/course-progress-routes');
-
+const getStudentViewCourseRoutes = require("./routes/student-routes/course-routes");
+const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
+const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(
-    cors({
-      origin:['http://localhost:5173', "https://lms-j8i4.onrender.com","https://bright-path-nu.vercel.app/"],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-      credentials: true,
-    })
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://lms-j8i4.onrender.com",
+//       "https://bright-path-nu.vercel.app",
+//     ],
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     credentials: true,
+//   })
+// );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://bright-path-nu.vercel.app"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 
-app.use((err,req,res,next)=>{
-    console.log(err.stack);
-    res.status(500).json({
-        success:false,
-        message:"Something went wrong"
-    })
-})
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
 // Database connection
 mongoose
-    .connect(MONGO_URI)
-    .then(() => console.log("mongodb is connected successfully"))
-    .catch((e)=>console.log(e));
+  .connect(MONGO_URI)
+  .then(() => console.log("mongodb is connected successfully"))
+  .catch((e) => console.log(e));
 
 // routes configuration
 
-app.use('/auth',authRoutes)
-app.use('/media',mediaRoutes)
-app.use('/instructor/course',instructorCourseRoutes)
-app.use('/student/course',getStudentViewCourseRoutes);
-app.use('/student/order',studentViewOrderRoutes);
-app.use('/student/courses-bought',studentCoursesRoutes);
-app.use('/student/course-progress',studentCourseProgressRoutes);
+app.use("/auth", authRoutes);
+app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
+app.use("/student/course", getStudentViewCourseRoutes);
+app.use("/student/order", studentViewOrderRoutes);
+app.use("/student/courses-bought", studentCoursesRoutes);
+app.use("/student/course-progress", studentCourseProgressRoutes);
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is now running on port ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
