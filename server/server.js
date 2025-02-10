fdc8716ce251b8e8c7e6c93fc3d98d8d2faba134
mohammedsuhail364 <mohammedthanish364@gmail.com>
2025-02-10 15:44:27 +0530

    new one

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://lms-pearl-eight.vercel.app/"
+    "https://lms-pearl-eight.vercel.app"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
