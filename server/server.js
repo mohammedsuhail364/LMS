c751721b2a04c2feb7b3f357d7d544fd71857468
mohammedsuhail364 <mohammedthanish364@gmail.com>
2026-02-21 21:58:22 +0530

    add frontend as static files

--- server/server.js
+++ server/server.js
@@ -2,22 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const authRoutes = require("./routes/auth-routes");
 const mediaRoutes = require("./routes/instructor-routes/mediaRoutes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/courseRoutes");
 const getStudentViewCourseRoutes = require("./routes/student-routes/course-routes");
 const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
-const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
+const __dirname = path.resolve();
 
 /* ✅ Use proper cors config */
 const corsOptions = {
-  origin: "https://lms-omega-rosy.vercel.app", // allow your Vercel frontend
+  origin: process.env.CLIENT_URL, // allow your Vercel frontend
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
@@ -49,6 +50,17 @@ app.use("/student/course", getStudentViewCourseRoutes);
 app.use("/student/order", studentViewOrderRoutes);
 app.use("/student/courses-bought", studentCoursesRoutes);
 app.use("/student/course-progress", studentCourseProgressRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const clientPath = path.join(__dirname, "../client/dist");
+
+  app.use(express.static(clientPath));
+
+  // SPA fallback (THIS is the key)
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
 
 /* ✅ Start server */
 app.listen(PORT, () => {
